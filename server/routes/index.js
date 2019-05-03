0d393347110488212446c7fbd88e7b864d139fb5
afdallismen <afdal.lismen@gmail.com>
2019-05-03 16:43:09 +0700

    Typo in routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,9 @@ const routes = require('express').Router()
 routes.use('/auth', require('./auth'))
 routes.use('/animals', require('./animals'))
 routes.use('/organizations', require('./organizations'))
-<<<<<<< HEAD
-routes.use('/wishes', require('./wishes'))
-=======
 // routes.use('/wishes', require('./wishes'))
 routes.use('/wishes', require('./wishes'))
 routes.use('/details', require('./detail'))
 // routes.use('/wishes', require('./wishes'))
->>>>>>> wiki trivia frontend
 
 module.exports = routes
