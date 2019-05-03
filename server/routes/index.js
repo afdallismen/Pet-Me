a73c9732f3de451d7b40cd67a78a53754e53954f
Dion <dionagoes@gmail.com>
2019-05-03 13:16:37 +0700

    fix routes/index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router()
 
 routes.use('/auth', require('./auth'))
+routes.use('/animals', require('./animals'))
+routes.use('/organizations', require('./organizations'))
 // routes.use('/wishes', require('./wishes'))
 
 module.exports = routes
